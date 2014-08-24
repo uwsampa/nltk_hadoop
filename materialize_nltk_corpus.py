e652b630ebbb8f7ab5a109b03d5f4f49f47853d8
Brandon Myers <bdmyers@cs.washington.edu>
2014-08-23 21:44:57 -0700

    utility to put an nltk corpus into files

---
+++ materialize_nltk_corpus.py
@@ -0,0 +1,32 @@
+import nltk
+import os
+import errno
+import sys
+import nltk.corpus
+
+corpusname = "inaugural"
+if len(sys.argv) >= 2:
+  corpusname = sys.argv[1]
+
+filelim = 4
+if len(sys.argv) >= 3:
+  filelim = int(sys.argv[2])
+
+corpus = getattr(nltk.corpus, corpusname)  
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: 
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+
+path = "./%s" % corpusname
+mkdir_p(path)
+
+
+for i in range(0,filelim):
+  fid = corpus.fileids()[i]
+  with open("%s/%s"%(path,fid), 'w') as out:
+    out.write(corpus.raw(fid))
