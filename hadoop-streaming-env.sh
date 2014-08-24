c42cc09d42127091ecacaf06d92d1f3a7397b58e
Brandon Myers <bdmyers@cs.washington.edu>
2014-08-23 21:46:24 -0700

    oops forgot the source file

---
+++ hadoop-streaming-env.sh
@@ -0,0 +1,4 @@
+export HADOOP_HOME=/sampa/share/hadoop-2.4.1
+export RELATIVE_PATH_JAR=share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar
+#HADOOP_HOME=/sampa/share/hadoop-1.0.3
+#RELATIVE_PATH_JAR=contrib/streaming/hadoop-streaming-1.0.3.jar
