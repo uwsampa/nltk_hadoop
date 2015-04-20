6d7263898cd1e7afac9c4d47a2a97d00f0d4ccbb
Zach Simon <zbsimon@cs.washington.edu>
2015-04-19 17:18:51 -0700

    changed initial patent-id / claims delimiter

--- claims_mapper.py
+++ claims_mapper.py
@@ -4,16 +4,17 @@ import sys
 import map_reduce_utils as mru
 
 # this should become an arg to map_claims
-INPUT_KV_DELIMITER = '~~'
+INPUT_KV_DELIM = '"~~'
 
-def map_claims(input=sys.stdin, output=sys.stdout, kv_delim=INPUT_KV_DELIMITER):
+
+def map_claims(input=sys.stdin, output=sys.stdout, kv_delim=INPUT_KV_DELIM):
     for line in input:
         key, value = line.strip().split(kv_delim)
-        filename = key.strip()
-        contents = mru.clean_text(value);
-        key = {'filename': filename}
+        patent_id = key.strip()
+        contents = mru.clean_text(value)
+        key = {'filename': patent_id}
         contents = {'words': [word for word in contents]}
-        mru.reducer_emit(key, value, output)
+        mru.reducer_emit(patent_id, value, output)
 
 
 if __name__ == '__main__':
