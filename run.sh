c8849c76c48e79aa0b69cb61586b46559b02edec
Zach Simon <zbsimon@cs.washington.edu>
2015-05-08 14:50:55 -0700

    add stop words file to run.sh command
    now `./run.sh` will use the stopwords in stopwords.txt instead of the
    default nltk ones

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 source /shared/patents/nltk-hadoop/settings.sh
 source /shared/patents/settings.sh
-./mapred_tfidf.py -i hdfs:///patents/claims_splits -o hdfs:///patents/output/similarities
+./mapred_tfidf.py -i hdfs:///patents/claims_splits -o hdfs:///patents/output/similarities -s stopwords.txt
