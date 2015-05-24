150096737a152cc25ea7edf9bc3976e807ad6ef3
Zach Simon <zbsimon@cs.washington.edu>
2015-05-24 15:33:38 -0700

    rename from nltk-hadoop to nltk_hadoop
    I need to use relative imports, so I'm renaming the project nltk_hadoop
    because python doesn't like when you import nltk-hadoop since it has a space.

--- settings.sh
+++ settings.sh
@@ -3,7 +3,7 @@
 export HADOOP_VERSION=2.6.0
 export AVRO_VERSION=1.7.7
 export HADOOP_HOME=/shared/hadoop/current
-export NLTK_HOME=/shared/patents/nltk-hadoop
+export NLTK_HOME=/shared/patents/nltk_hadoop
 export HADOOP_JAR=$HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-$HADOOP_VERSION.jar
 export AVRO_JAR=$NLTK_HOME/lib/avro-mapred-$AVRO_VERSION.jar
 #export AVRO_JAR=$HADOOP_HOME/share/hadoop/common/lib/avro-mapred-$AVRO_VERSION.jar
