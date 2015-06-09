ac9e8bbe366326eaedbcc517acce98281dfb50ef
Brandon Myers <bdmyers@cs.washington.edu>
2015-06-09 12:46:20 -0700

    add tfidf result catalog

--- radish/catalog.py
+++ radish/catalog.py
@@ -1,3 +1,6 @@
+# Schemas corresponding to Myrial examples
+
 {
-    'public:adhoc:bigrams': [('term','STRING_TYPE'), ('filename', 'STRING_TYPE')],
-   }
+    'public:adhoc:bigrams' : [('term', 'STRING_TYPE'), ('filename','STRING_TYPE')],
+    'public:adhoc:tfidf' : [('term', 'STRING_TYPE'), ('document', 'STRING_TYPE'), ('tfidf', 'LONG_TYPE')],
+}
