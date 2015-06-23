3bd95b396d402dc79e48a2ae9fd02f30335f3943
Brandon Myers <bdmyers@cs.washington.edu>
2015-06-23 15:51:39 -0700

    fixes to the query and catalog

--- radish/catalog.py
+++ radish/catalog.py
@@ -1,6 +1,6 @@
 # Schemas corresponding to Myrial examples
 
 {
-    'public:adhoc:bigrams' : [('term', 'STRING_TYPE'), ('filename','STRING_TYPE')],
+    'public:adhoc:ngrams' : [('word', 'STRING_TYPE'), ('filename','STRING_TYPE')],
     'public:adhoc:tfidf' : [('term', 'STRING_TYPE'), ('document', 'STRING_TYPE'), ('tfidf', 'LONG_TYPE')],
 }
